c51c3638bbcf2ee75f50f4a8d546164b50b663cf
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2024-02-22 12:45:45 +0200

    delete 6 files

--- src/data_server/api/__init__.py
+++
@@ -1,5 +0,0 @@
-from .photos.router import PhotosRouter
-from .users.router import UserRouter
-from .messages.router import MessagesRouter
-
-__all__ = ["PhotosRouter", "UserRouter", "MessagesRouter"]
