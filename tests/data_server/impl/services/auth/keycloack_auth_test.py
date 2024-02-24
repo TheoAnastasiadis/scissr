646a1ee902b7e13f0c6eb55ede9a5dde660619b2
Theo Anastasiadis <theoanastasiadis98@gmail.com>
2024-02-24 18:05:49 +0200

    test: implemented keycloack tests

--- tests/data_server/impl/services/auth/keycloack_auth_test.py
+++ tests/data_server/impl/services/auth/keycloack_auth_test.py
@@ -1,5 +1,73 @@
+from fastapi import HTTPException
+from src.data_server.domain.services.db.user import UserDB
 from src.data_server.impl.services.auth.keyclock_auth import KeyCloackAuth
+from keycloak import KeycloakOpenID
+import pytest
+from unittest.mock import MagicMock
 
 
-def test_raise_nie():
-    raise NotImplementedError("missing")
+@pytest.fixture
+def user_db():
+    return MagicMock(spec=UserDB)
+
+
+@pytest.fixture
+def keycloak_client():
+    return MagicMock(sepc=KeycloakOpenID)
+
+
+@pytest.fixture
+def keycloak_auth(
+    user_db,
+    keycloak_client,
+):
+    return KeyCloackAuth(
+        user_db,
+        keycloak_client,
+    )
+
+
+def test_get_payload_success(keycloak_auth, keycloak_client):
+    token = "dummy_token"
+    keycloak_client.decode_token.return_value = {
+        "sub": "123",
+        "email": "test@example.com",
+        "preferred_username": "test_user",
+    }
+    payload = keycloak_auth._get_payload(token)
+    assert payload == {
+        "sub": "123",
+        "email": "test@example.com",
+        "preferred_username": "test_user",
+    }
+
+
+def test_get_payload_failure(keycloak_auth, keycloak_client):
+    token = "dummy_token"
+    keycloak_client.decode_token.side_effect = Exception(
+        "Token decoding failed"
+    )
+    with pytest.raises(HTTPException):
+        keycloak_auth._get_payload(token)
+
+
+def test_get_caller(keycloak_auth, keycloak_client):
+    token = "dummy_token"
+    keycloak_client.decode_token.return_value = {
+        "sub": "123",
+        "email": "test@example.com",
+        "preferred_username": "test_user",
+    }
+    caller = keycloak_auth.get_caller(token)
+    assert caller.sub == "123"
+    assert caller.email == "test@example.com"
+    assert caller.p_username == "test_user"
+
+
+def test_get_caller_failure(keycloak_auth, keycloak_client):
+    token = "dummy_token"
+    keycloak_client.decode_token.side_effect = Exception(
+        "Token decoding failed"
+    )
+    with pytest.raises(HTTPException):
+        keycloak_auth.get_caller(token)
