774244793ff44024620e41b7f39b1bb5077f8f07
Theo Anastasiadis <theoanastasiadis98@gmail.com>
2024-02-29 19:11:32 +0200

    feat(ui): update tailwind.config.ts

--- src/client/tailwind.config.ts
+++ src/client/tailwind.config.ts
@@ -1,9 +1,11 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -12,68 +14,24 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors: {
-        black: {
-          500: "#130720",
-          900: "#021002",
-        },
-        primary: {
-          1000: "#5e10a8",
-          900: "#6e28b1",
-          800: "#7e40b9",
-          700: "#8e58c2",
-          600: "#9e70cb",
-          500: "#af88d4",
-          400: "#bf9fdc",
-          300: "#cfb7e5",
-          200: "#dfcfee",
-          100: "#efe7f6",
-        },
-        sucess: {
-          1000: "#237429",
-          900: "#39823e",
-          800: "#4f9054",
-          700: "#659e69",
-          600: "#7bac7f",
-          500: "#91ba94",
-          400: "#a7c7a9",
-          300: "#bdd5bf",
-          200: "#d3e3d4",
-          100: "#e9f1ea",
-        },
-        info: {
-          1000: "#ffe3f1",
-          900: "#e6ccd9",
-          800: "#ccb6c1",
-          700: "#b39fa9",
-          600: "#998891",
-          500: "#807279",
-          400: "#665b60",
-          300: "#4c4448",
-          200: "#332d30",
-          100: "#191718",
-        },
-        error: {
-          1000: "#ae455d",
-          900: "#9d3e54",
-          800: "#8b374a",
-          700: "#7a3041",
-          600: "#682938",
-          500: "#57232f",
-          400: "#461c25",
-          300: "#34151c",
-          200: "#230e13",
-          100: "#110709",
-        },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
-      fontFamily: {
-        "flow-circular": ['"Flow Circular"', "system-ui"],
-        archivo: ['"Archivo Black"', "sans-serif"],
-        shadows: ['"Shadows Into Light"', "cursive"],
-        protest: ['"Protest Guerrilla"', "sans-serif"],
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0%",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "100%",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
